62dc3102575aef2a989fbc9eef2b1a7da7fc5b8f
Jainritgithub <riteshjn022@gmail.com>
2024-04-04 17:11:03 +0530

    css-implementation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './style.module.css'
+const App = () => {
+  return (
+    <div>
+      <p className={css.paracolor}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita adipisci vitae ducimus vel, explicabo voluptates quidem amet ratione tempore repellat voluptate corporis beatae eaque qui.</p>
+    </div>
+  )
+}
+
+export default App
